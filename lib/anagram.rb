9d4c5430a2da6d7a8ecf2dfa0d0ca2af4460bb59
Daniel-Mack <daniel.mack@powerhrg.com>
2018-05-23 20:20:47 -0400

    Built anagram program

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,22 @@
 # Your code goes here!
+require 'pry'
+
+class Anagram
+
+  attr_accessor :word
+
+  def initialize(word)
+    @word = word
+  end
+
+  def match(words)
+    anagram = []
+     words.each do |words_array|
+      words_array == [""]
+      if words_array.split("").sort == word.split("").sort
+        anagram << words_array
+      end
+     end
+    anagram
+  end
+end
